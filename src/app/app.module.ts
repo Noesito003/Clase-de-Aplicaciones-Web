0b15c7e0070d62b957ba7f6cdcbb48829aea7be6
Yahir <2121100556@soy.utj.edu.mx>
2023-02-08 08:23:01 -0600

    Routing And Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductoComponent } from './producto/producto.component';
+import { LoginService } from './services/login.service';
+import { ClienteService } from './services/cliente.service';
+import { ProductoService } from './services/producto.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    ContactoComponent,
+    DirectivasComponent,
+    EmpleadoComponent,
+    EncabezadoComponent,
+    InicioComponent,
+    LoginComponent,
+    MenuComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    ClienteService,
+    ProductoService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
