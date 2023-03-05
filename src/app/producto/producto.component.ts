5fc44d5b1df4317c6fe021fff4a9cd2feff7e12f
Yahir Figueroa <2121100556@soy.utj.edu.mx>
2023-03-04 21:43:00 -0600

    First steep for registrer a product

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
   selector: 'app-producto',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+
+  constructor(private router: Router ,private producto: ProductoService) { }
+
+  product = {
+    name: '',
+    description: '',
+    price: 0,
+    image: '',
+    category: [],
+    size: [],
+  }
+
+
+  setProduct = () => {
+    this.producto.setProduct(this.product).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+
+
+    )
+  }
 
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/clothe';
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  setProduct = (product: object) => {
+    return this.http.post<any>(this.url, product);
+  }
+
 }
